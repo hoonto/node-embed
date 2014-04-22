b24c1548cfa1b80d6706ebba540c626410c13e93
Matt Mullens <mmullens@hoonto.com>
2014-04-21 21:55:16 -0700

    make sure libnode.so is in the library path, and also note that the emit is not yet working - it is being fired before the process.on - also this is on v0.10.12

--- example.cc
+++ example.cc
@@ -6,27 +6,26 @@
 
 using namespace v8;
 
-int
-main (void)
-{   int a = 3;
-    std::cout << "++a is: " << ++a << " and a++ is: " << a++ << std::endl;
- 
-    return 0; 
-}
 
-static v8::Local<v8::Context>
-GetNodeContext()
+
+int main (int argc, char *argv[])
 {
-    char *argv[] = {"plnode", NULL};
-    int argc = sizeof(argv) / sizeof(char*) - 1;
-
-
     v8::Handle<v8::ObjectTemplate>  globaltemplate;
     v8::Handle<v8::Object> processref;
-    v8::Local<v8::Context> contextref;
-    char** argvcopyref;
+    v8::Persistent<v8::Context> contextref;
 
-    node::buildContext(argc, argv, globaltemplate, contextref, processref, argvcopyref);
+    node::buildContext(argc, argv, globaltemplate, contextref, processref, argv);
+    node::runContext(contextref,processref,argv);
 
-	return contextref;
+    v8::Local<v8::Value> emit_v = processref->Get(String::New("emit"));
+    assert(emit_v->IsFunction());
+    v8::Local<v8::Function> emit = v8::Local<v8::Function>::Cast(emit_v);
+    v8::Local<v8::Value> args[] = { v8::String::New("hello"), v8::Integer::New(0) };
+    TryCatch try_catch;
+    emit->Call(processref, 2, args);
+    if (try_catch.HasCaught()) {
+        node::FatalException(try_catch);
+    }
+
+    return 0;
 }
---
+++ test.js
@@ -0,0 +1,10 @@
+process.on('hello',function() {
+    console.log('world!!')
+})
+
+console.log('HEY FROM NODE!!')
+setInterval(function() {},10000)
+
+function testFunc(){
+    return 232;
+}
